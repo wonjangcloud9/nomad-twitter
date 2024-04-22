3bfba0fda4fa9a600ab566d0cc8b978660c31467
wonjangcloud9 <venuscwj@naver.com>
2024-04-22 18:11:12 +0900

    🎉 Project init
    프로젝트 시작

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const { relative } = require("path");
+
+module.exports = {
+  relative: true,
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
