5fda6a744cb8a4110894c6133d85b35300741f91
wonjangcloud9 <venuscwj@naver.com>
2024-04-26 11:59:05 +0900

    🐛 Vercel 배포를 위한 수정
    middleware 수정

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,4 +1,4 @@
-import type { NextRequest, NextFetchEvent } from "next/server";
-export function middleware(req: NextRequest, ev: NextFetchEvent) {
+import type { NextRequest } from "next/server";
+export function middleware(req: NextRequest) {
   console.log(req.ua);
 }
