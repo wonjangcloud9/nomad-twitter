b48cbc4a24f42f098db60cdebb4934eacaaf18f2
wonjangcloud9 <venuscwj@naver.com>
2024-04-29 03:01:34 +0900

    ⚡️ 명언 개선
    명언 개선 ㅋㅋ

--- pages/_components/contents.tsx
+++ pages/_components/contents.tsx
@@ -1,15 +1,6 @@
 import useSWR from "swr";
-import { timeAgo } from "../../lib/client/utils";
+import { timeAgo, wiseSayings } from "../../lib/client/utils";
 import Link from "next/link";
-
-const randomWiseSayings = [
-  "아무것도 하지 않으면 아무것도 일어나지 않는다.",
-  "시작이 반이다.",
-  "행동이 모든 것을 바꾼다.",
-  "끝까지 가는 것이 중요하다.",
-  "포기하지 않는 한 성공한다.",
-  "끝까지 가면 반드시 성공한다",
-];
 
 interface ContentsProps {
   username: string | undefined;
@@ -39,15 +30,12 @@ export default ({ username }: ContentsProps) => {
       <div className="pt-12  bg-white shadow-xl px-3 w-96">
         <h1 className="text-2xl font-bold mb-2">안녕하세요, {username}님</h1>
         <p className="text-sm text-gray-500 mb-2">
-          {
-            randomWiseSayings[
-              Math.floor(Math.random() * randomWiseSayings.length)
-            ]
-          }
+          {wiseSayings[Math.floor(Math.random() * wiseSayings.length)]}
         </p>
         {data &&
           data?.tweets.map((tweet) => {
             const tweetId = tweet.id;
+            console.log(tweet);
             return (
               <Link href={`/tweets/${tweetId}`} key={tweet.id}>
                 <div className="border-b border-gray-200 py-2 cursor-pointer hover:bg-gray-100 transition-colors duration-200 px-2 rounded-md">
