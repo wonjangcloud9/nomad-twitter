3d6b26cac5feabf961719d9ac59d7b605f6110bc
wonjangcloud9 <venuscwj@naver.com>
2024-04-29 03:31:33 +0900

    ✨ 배포만 가보자 디테일페이지
    디테일

--- pages/api/tweets/[id]/index.ts
+++ pages/api/tweets/[id]/index.ts
@@ -1,15 +1,37 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { withApiSession } from "../../../../lib/server/withSession";
 import withHandler from "../../../../lib/server/withHandler";
+import db from "../../../../lib/server/db";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const {
     query: { id },
     session: { user },
   } = req;
-  console.log("Hello", id, user);
+  const tweet = await db.tweet.findUnique({
+    where: { id: +id.toString() },
+    include: {
+      user: {
+        select: { name: true, id: true },
+      },
+    },
+  });
+  if (!tweet) {
+    return res.status(404).json({ ok: false });
+  }
+  const favs = await db.fav.findFirst({
+    where: {
+      tweetId: tweet.id,
+      userId: user?.id,
+    },
+    select: { id: true },
+  });
 
-  res.json({ ok: true });
+  res.json({
+    ok: true,
+    tweet,
+    favs: favs ? [favs] : [],
+  });
 }
 
 export default withApiSession(
--- pages/tweets/[id].tsx
+++ pages/tweets/[id].tsx
@@ -1,14 +1,76 @@
 import React from "react";
 import Header from "../_components/header";
 // import useUser from "../../lib/client/useUser";
-// import { useRouter } from "next/router";
+import { useRouter } from "next/router";
+import useSWR from "swr";
+import { Fav, Tweet, User } from "@prisma/client";
+// import useMutation from "../../lib/client/useMutation";
+import { FaRegThumbsUp } from "react-icons/fa";
+
+interface TweetWithUser extends Tweet {
+  user: User;
+}
+
+interface TweetResponse {
+  ok: boolean;
+  tweet: TweetWithUser;
+  favs: Fav[];
+}
 
 export default () => {
+  // const { user, isLoading } = useUser();
+  const router = useRouter();
+  // const { mutate } = useSWRConfig();
+  const { data, mutate: boundMutate } = useSWR<TweetResponse>(
+    router.query.id ? `/api/tweets/${router.query.id}` : null
+  );
+  // const [toggleFav] = useMutation(`/api/tweets/${router.query.id}/fav`);
+
   return (
     <div className="flex justify-center min-h-screen">
       <Header />
-      <div className="pt-12  bg-white shadow-xl px-3 w-96 flex flex-col gap-12 justify-center">
-        <div className="text-2xl font-bold text-center">Tweet 만들기</div>
+      <div className="pt-12  bg-white shadow-xl px-3 w-96 flex flex-col gap-5 justify-center">
+        <div
+          onClick={() => router.back()}
+          className="cursor-pointer text-orange-500 "
+        >
+          뒤로가기
+        </div>
+        {data?.ok ? (
+          <>
+            <p className="text-lg bg-gray-100 p-3 rounded-lg cursor-pointer ">
+              {data.tweet.description}
+            </p>
+            <div className="flex gap-3 justify-between px-2 items-center">
+              <div
+                className="flex flex-col
+              justify-center items-center"
+              >
+                <span className="text-gray-500">@{data.tweet.user.name}</span>
+              </div>
+              <div>
+                <span className="text-gray-500">
+                  {new Date(data.tweet.createdAt).toLocaleString()}
+                </span>
+              </div>
+              <button
+                className="bg-orange-400 text-white p-2 rounded-lg hover:bg-orange-500 transition text-center w-9"
+                onClick={() => {
+                  // toggleFav();
+                  boundMutate();
+                }}
+              >
+                {data.favs.length > 0 ? (
+                  <FaRegThumbsUp className="text-white w-5 h-5" />
+                ) : (
+                  <FaRegThumbsUp className="text-white w-5 h-5" />
+                )}
+              </button>
+            </div>
+          </>
+        ) : (
+          <div>Loading...</div>
+        )}
       </div>
     </div>
   );
