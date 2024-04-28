7a2fd0cef459624ca321085811a418b9844bd04d
wonjangcloud9 <venuscwj@naver.com>
2024-04-29 01:18:25 +0900

    ⚡️ 코드정리
    코드정리

--- pages/api/users/me/index.ts
+++ pages/api/users/me/index.ts
@@ -5,30 +5,35 @@ import db from "../../../../lib/server/db";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "GET") {
-    if (!req.session.user) {
-      return res.status(401).json({
-        ok: false,
-        message: "Unauthorized",
+    try {
+      if (!req.session.user) {
+        return res.status(401).json({
+          ok: false,
+          message: "Unauthorized",
+        });
+      }
+
+      const profile = await db.user.findUnique({
+        where: {
+          id: req.session.user?.id,
+        },
       });
-    }
 
-    const profile = await db.user.findUnique({
-      where: {
-        id: req.session.user?.id,
-      },
-    });
+      if (!profile) {
+        return res.status(404).json({
+          ok: false,
+          message: "User not found",
+        });
+      }
 
-    if (!profile) {
-      return res.status(404).json({
-        ok: false,
-        message: "User not found",
+      return res.json({
+        ok: true,
+        profile,
       });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ error });
     }
-
-    return res.json({
-      ok: true,
-      profile,
-    });
   }
 }
 
