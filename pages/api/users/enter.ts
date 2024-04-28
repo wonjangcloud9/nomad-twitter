f5a8d1d34e66d2dba403ca055db8ef8770667f3f
wonjangcloud9 <venuscwj@naver.com>
2024-04-28 19:41:50 +0900

    ✨ 회원가입 시 정보 들어가는거 확인
    회원가입 시 정보 들어가도록 변경

--- pages/api/users/enter.ts
+++ pages/api/users/enter.ts
@@ -1,9 +1,34 @@
 import withHandler from "../../../lib/server/withHandler";
 import { NextApiResponse, NextApiRequest } from "next";
+import db from "../../../lib/server/db";
+import bcrypt from "bcrypt";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
-  res.status(200).json({ ok: true });
+  const { email, nickname: name, password } = req.body;
+
+  console.log(email, name, password);
+
+  if (!email || !name || !password) {
+    return res.status(400).json({ error: "Missing fields" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await db.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  });
+
+  if (!user) {
+    return res.status(500).json({ error: "Failed to create user" });
+  }
+
+  return res.json({
+    ok: true,
+  });
 }
 
 export default withHandler({ methods: ["POST"], handler });
---
+++ prisma/migrations/20240428103234_token_and/migration.sql
@@ -0,0 +1,33 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Token` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the column `phone` on the `User` table. All the data in the column will be lost.
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropIndex
+DROP INDEX "Token_payload_key";
+
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "Token";
+PRAGMA foreign_keys=on;
+
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT,
+    "name" TEXT NOT NULL,
+    "avatar" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "password" TEXT NOT NULL
+);
+INSERT INTO "new_User" ("avatar", "createdAt", "email", "id", "name", "updatedAt") SELECT "avatar", "createdAt", "email", "id", "name", "updatedAt" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
