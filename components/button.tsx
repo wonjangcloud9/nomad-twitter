fcbdc3e77e38a03ad771f58716299aa424234273
wonjangcloud9 <venuscwj@naver.com>
2024-04-29 04:49:13 +0900

    ⚡️ Login & enter btn 로딩중일 때 disabled
    제곧내

--- components/button.tsx
+++ components/button.tsx
@@ -21,6 +21,7 @@ export default function Button({
         "bg-orange-400 hover:bg-orange-500 text-white px-4 border border-transparent rounded-xl shadow-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none transition w-80",
         large ? "py-3 text-base" : "py-2 text-sm "
       )}
+      disabled={loading}
     >
       {loading ? "로딩중..." : text}
     </button>
